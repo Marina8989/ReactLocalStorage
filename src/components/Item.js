b043d38ec631cab85395f36eb657dc3c0aed5910
Marina8989 <marina.mill209@gmail.com>
2021-09-10 23:01:16 -0700

    react

--- src/components/Item.js
+++ src/components/Item.js
@@ -6,6 +6,13 @@ const Item = (props) => {
           {props.item.value}
           <button onClick={() => props.handleToggle(props.item)} style={{marginLeft: '10px', color: 'green'}}>toggle</button>
           <button onClick={() => props.handleRemove(props.item)} style={{marginLeft: '10px', color: 'green'}}>remove</button>
+          <select style={{color: 'green'}}>
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={4}>4</option>
+              <option value={5}>5</option>
+          </select>
         </li>
     )
 }
